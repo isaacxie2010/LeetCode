97c67cc36eeb129a508e5f0f3e4f533d63153594
Isaac Xie <ixie@quisk.co>
2016-04-25 22:32:35 -0700

    Problem 326: Power of Three

---
+++ com/isaacxie/leetcode/PowerOfThree.java
@@ -0,0 +1,17 @@
+package com.isaacxie.leetcode;
+
+public class PowerOfThree {
+	//https://leetcode.com/problems/power-of-three/submissions/
+	
+	public boolean isPowerOfThree(int n) {
+		// log3(n) = log10(n)/log10(3) 
+        return (Math.log10(n)/Math.log10(3)) % 1 == 0;
+    }
+	
+	public boolean isPowerOfThreeIntuitive(int n) {
+		while(n > 1 && n % 3 == 0) {
+			n = n / 3;
+		}
+		return n == 1;
+	}
+}
