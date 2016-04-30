3ef144eb96c63a821fd0be2d06133a11d680d4ed
Isaac Xie <isaacxie1989@gmail.com>
2016-04-30 10:47:56 -0700

    Problem 235: Lowest Common Ancestor of a Binary Search Tree

---
+++ com/isaacxie/leetcode/LowestCommonAncesterOfABinarySearchTree.java
@@ -0,0 +1,25 @@
+package com.isaacxie.leetcode;
+
+public class LowestCommonAncesterOfABinarySearchTree {
+	// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+		if (root.val > p.val && root.val > q.val) {
+			return lowestCommonAncestor(root.left, p, q);
+		} else if (root.val < p.val && root.val < q.val) {
+			return lowestCommonAncestor(root.right, p, q);
+		} else {
+			return root;
+		}
+	}
+
+	class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+}
