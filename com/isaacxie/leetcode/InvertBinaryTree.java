a09aa5afa158503e23a870f86cfa1cdd57999087
Isaac Xie <isaacxie1989@gmail.com>
2016-05-04 21:59:28 -0700

    Problem 226: Invert Binary Tree

---
+++ com/isaacxie/leetcode/InvertBinaryTree.java
@@ -0,0 +1,28 @@
+package com.isaacxie.leetcode;
+
+public class InvertBinaryTree {
+	// https://leetcode.com/problems/invert-binary-tree/
+
+	public TreeNode invertTree(TreeNode root) {
+		if (root == null) {
+			return null;
+		}
+		TreeNode temp = root.left;
+		root.left = root.right;
+		root.right = temp;
+
+		invertTree(root.left);
+		invertTree(root.right);
+		return root;
+	}
+
+	class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+}
