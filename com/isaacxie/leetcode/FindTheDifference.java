a8c261dcf8aaccc392983e2f3fd109d62de3a553
Isaac Xie <ixie@quisk.co>
2016-08-29 20:54:10 -0700

    389. Find the Difference

---
+++ com/isaacxie/leetcode/FindTheDifference.java
@@ -0,0 +1,15 @@
+package com.isaacxie.leetcode;
+
+public class FindTheDifference {
+	// https://leetcode.com/problems/find-the-difference/
+	public char findTheDifference(String s, String t) {
+		char result = 0;
+		for(int i = 0; i < s.length(); i++) {
+			result ^= s.charAt(i);
+		}
+		for(int i = 0; i < t.length(); i++) {
+			result ^= t.charAt(i);
+		}
+		return result;
+	}
+}
