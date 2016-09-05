3d0ece8fdbacac45a5635c39378589210a3108b5
Isaac Xie <ixie@quisk.co>
2016-09-05 10:34:57 -0700

    374. Guess Number Higher or Lower

--- com/isaacxie/leetcode/CombinationSumIV.java
+++ com/isaacxie/leetcode/CombinationSumIV.java
@@ -1,13 +1,17 @@
 package com.isaacxie.leetcode;
 
+import java.util.Arrays;
+
 public class CombinationSumIV {
+	// https://leetcode.com/problems/combination-sum-iv/
+
 	public int combinationSum4(int[] nums, int target) {
 		Arrays.sort(nums);
 		int[] arr = new int[target + 1];
 		arr[0] = 1;
 		for (int i = 1; i <= target; i++) {
 			for (int num : nums) {
-				if (i >= num) {
+				if (num <= i) {
 					arr[i] += arr[i - num];
 				}
 			}
