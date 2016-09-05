e172bda47a220422c9aca0fce0e8c4970ace87ba
Isaac Xie <ixie@quisk.co>
2016-09-05 10:04:41 -0700

    376. Wiggle Subsequence

--- com/isaacxie/leetcode/WiggleSubsequence.java
+++ com/isaacxie/leetcode/WiggleSubsequence.java
@@ -4,6 +4,7 @@ public class WiggleSubsequence {
 	// https://leetcode.com/problems/wiggle-subsequence/
 
 	public int wiggleMaxLength(int[] nums) {
+		// greedy method
 		if (nums == null || nums.length == 0) {
 			return 0;
 		}
@@ -17,5 +18,27 @@ public class WiggleSubsequence {
 			}
 		}
 		return Math.max(countInc, countDec);
+	}
+
+	public int wiggleMaxLength2(int[] nums) {
+		// turning points method
+		if (nums == null || nums.length == 0) {
+			return 0;
+		}
+		int count = 1;
+		for (int i = 1; i < nums.length; i++) {
+			if (nums[i] > nums[i - 1]) {
+				count++;
+				while (i < nums.length - 1 && nums[i + 1] >= nums[i]) {
+					i++;
+				}
+			} else if (nums[i] < nums[i - 1]) {
+				count++;
+				while (i < nums.length - 1 && nums[i + 1] <= nums[i]) {
+					i++;
+				}
+			}
+		}
+		return count;
 	}
 }
