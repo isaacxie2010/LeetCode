5701d4e83c6a0907c34e794fe3e598dd4654e326
Isaac Xie <zhoxie@paypal.com>
2018-04-27 22:01:37 -0700

    Problem 542. 01Matrix

--- com/isaacxie/leetcode/_01_Matrix.java
+++ com/isaacxie/leetcode/_01_Matrix.java
@@ -7,39 +7,37 @@ import java.util.Queue;
 public class _01_Matrix {
 	// https://leetcode.com/problems/01-matrix/
 	
-	public List<List<Integer>> updateMatrix(List<List<Integer>> matrix) {
+	public int[][] updateMatrix(int[][] matrix) {
 		// BFS, intial queue with all 0s, and set matrix not-zero element
 		// Integer.MAX
-		int x = matrix.size();
-		int y = matrix.get(0).size();
-		Queue<int[]> queue = new LinkedList<>();
-		for (int i = 0; i < x; i++) {
-			for (int j = 0; j < y; j++) {
-				if (matrix.get(i).get(j) == 0) {
-					queue.offer(new int[] { i, j });
-				} else {
-					matrix.get(i).set(j, Integer.MAX_VALUE);
-				}
-			}
-		}
-		int[][] directions = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
-		while (!queue.isEmpty()) {
-			int[] elements = queue.poll();
-			for (int[] dir : directions) {
-				int r = elements[0] + dir[0];
-				int c = elements[1] + dir[1];
-				if (r >= x || r < 0 || c >= y || c < 0) {
-					// out of bound
-					continue;
-				}
-				if (matrix.get(r).get(c) <= matrix.get(elements[0]).get(elements[1]) + 1) {
-					// means no need to update
-					continue;
-				}
-				queue.offer(new int[] { r, c });
-				matrix.get(r).set(c, matrix.get(elements[0]).get(elements[1]) + 1);
-			}
-		}
-		return matrix;
-	}
+        int x = matrix.length;
+        int y = matrix[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                if (matrix[i][j] == 0) {
+                    queue.offer(new int[]{i, j});
+                } else {
+                    matrix[i][j] = Integer.MAX_VALUE;
+                }
+            }
+        }
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        while(!queue.isEmpty()) {
+            int[] item = queue.poll();
+            for(int[] dir : directions) {
+                int r = item[0] + dir[0];
+                int c = item[1] + dir[1];
+                if(r >= x || r < 0 || c >= y || c < 0) {
+                    continue;  // out of bound
+                }
+                if(matrix[r][c] <= matrix[item[0]][item[1]] + 1) {
+                    continue; // not need to update
+                }
+                matrix[r][c] = matrix[item[0]][item[1]] + 1;
+                queue.offer(new int[]{r, c});
+            }
+        }
+        return matrix;
+    }
 }
